98fa33c4e49120eed5e5f7eac8287ac1496b93ad
Maxwell Anderson <maxwell.anderson.public@protonmail.com>
2022-03-12 22:44:48 -0700

    test conditional compilation

--- src/main.rs
+++ src/main.rs
@@ -5,132 +5,19 @@ use std::{
     io::{self, stdout, BufRead, BufReader},
 };
 
+#[cfg(target_os = "android")]
+use std::process::Command;
+
 use crossterm::{
     style::{style, Attribute, Color, ResetColor, Stylize},
     ExecutableCommand, Result,
 };
 
-fn get_os_info(os_release: &str, key: &str) -> String {
-    if let Some(pretty_name) = os_release
-        .split('\n')
-        .map(|line| line.split('=').collect::<Vec<_>>())
-        .find(|line| line[0] == key)
-    {
-        pretty_name[1].trim_matches('"').to_owned()
-    } else {
-        "Linux".to_owned()
-    }
-}
-
-fn get_kver() -> String {
-    if let Ok(ver) = fs::read_to_string("/proc/version") {
-        ver.split(' ').nth(2).unwrap_or("linux").to_owned()
-    } else {
-        "linux".to_owned()
-    }
-}
-
-fn get_shell(envvars: &HashMap<String, String>) -> String {
-    if let Some(var) = envvars.get("SHELL") {
-        if let Some(last_slash) = var.rfind('/') {
-            &var[last_slash + 1..]
-        } else {
-            var
-        }
-    } else {
-        "sh"
-    }
-    .to_owned()
-}
-
-fn get_uptime() -> String {
-    if let Ok(uptime) = fs::read_to_string("/proc/uptime") {
-        let mut seconds = uptime
-            .split(' ')
-            .next()
-            .unwrap_or("0")
-            .to_owned()
-            .parse::<f64>()
-            .unwrap_or(0.0) as i32;
-
-        let days = seconds / 86400;
-        seconds -= days * 86400;
-
-        let hours = seconds / 3600;
-        seconds -= hours * 3600;
-
-        let minutes = seconds / 60;
-        seconds -= minutes * 60;
-
-        format!(
-            "{}{}{}{}s",
-            if days == 0 {
-                String::default()
-            } else {
-                format!("{}d ", days)
-            },
-            if hours == 0 {
-                String::default()
-            } else {
-                format!("{}h ", hours)
-            },
-            if minutes == 0 {
-                String::default()
-            } else {
-                format!("{}m ", minutes)
-            },
-            seconds
-        )
-    } else {
-        "0 s".to_owned()
-    }
-}
-
-fn get_mem() -> io::Result<String> {
-    let meminfo = File::open("/proc/meminfo")?;
-    let reader = BufReader::new(meminfo).lines();
-
-    let mut total = 0;
-    let mut free = 0;
-    let mut buffers = 0;
-    let mut cached = 0;
-
-    for line in reader {
-        let l: Vec<String> = line?.split_whitespace().map(str::to_owned).collect();
-        match l[0].as_str() {
-            "MemTotal:" => {
-                total = l[1].parse::<i32>().unwrap_or(0);
-            }
-            "MemFree:" => {
-                free = l[1].parse::<i32>().unwrap_or(0);
-            }
-            "Buffers" => {
-                buffers = l[1].parse::<i32>().unwrap_or(0);
-            }
-            "Cached:" => {
-                cached = l[1].parse::<i32>().unwrap_or(0);
-                break;
-            }
-            _ => (),
-        }
-    }
-
-    Ok(format!(
-        "{}M / {}M",
-        (total - free - buffers - cached) / 1024,
-        total / 1024
-    ))
-}
-
 fn main() -> Result<()> {
     let args: Vec<_> = env::args().collect();
     let envvars: HashMap<_, _> = env::vars().collect();
 
-    let username = if let Some(var) = envvars.get("USER") {
-        var
-    } else {
-        "user"
-    };
+    let username = get_user(&envvars);
     let hostname = if let Ok(s) = fs::read_to_string("/etc/hostname") {
         s.trim().to_owned()
     } else {
@@ -300,4 +187,129 @@ fn main() -> Result<()> {
     stdout().execute(ResetColor)?;
 
     Ok(())
+}
+
+fn get_user(envvars: &HashMap<String, String>) -> String {
+    if let Some(var) = envvars.get("USER") {
+        var.to_owned()
+    } else {
+        "user".to_owned()
+    }
+}
+
+#[cfg(not(target_os = "linux"))]
+fn get_user(envvars: &HashMap<String, String>) -> String {
+    Command::new("whoami").output().unwrap_or("user".to_owned())
+}
+
+fn get_os_info(os_release: &str, key: &str) -> String {
+    if let Some(pretty_name) = os_release
+        .split('\n')
+        .map(|line| line.split('=').collect::<Vec<_>>())
+        .find(|line| line[0] == key)
+    {
+        pretty_name[1].trim_matches('"').to_owned()
+    } else {
+        "Linux".to_owned()
+    }
+}
+
+fn get_kver() -> String {
+    if let Ok(ver) = fs::read_to_string("/proc/version") {
+        ver.split(' ').nth(2).unwrap_or("linux").to_owned()
+    } else {
+        "linux".to_owned()
+    }
+}
+
+fn get_shell(envvars: &HashMap<String, String>) -> String {
+    if let Some(var) = envvars.get("SHELL") {
+        if let Some(last_slash) = var.rfind('/') {
+            &var[last_slash + 1..]
+        } else {
+            var
+        }
+    } else {
+        "sh"
+    }
+    .to_owned()
+}
+
+fn get_uptime() -> String {
+    if let Ok(uptime) = fs::read_to_string("/proc/uptime") {
+        let mut seconds = uptime
+            .split(' ')
+            .next()
+            .unwrap_or("0")
+            .to_owned()
+            .parse::<f64>()
+            .unwrap_or(0.0) as i32;
+
+        let days = seconds / 86400;
+        seconds -= days * 86400;
+
+        let hours = seconds / 3600;
+        seconds -= hours * 3600;
+
+        let minutes = seconds / 60;
+        seconds -= minutes * 60;
+
+        format!(
+            "{}{}{}{}s",
+            if days == 0 {
+                String::default()
+            } else {
+                format!("{}d ", days)
+            },
+            if hours == 0 {
+                String::default()
+            } else {
+                format!("{}h ", hours)
+            },
+            if minutes == 0 {
+                String::default()
+            } else {
+                format!("{}m ", minutes)
+            },
+            seconds
+        )
+    } else {
+        "0 s".to_owned()
+    }
+}
+
+fn get_mem() -> io::Result<String> {
+    let meminfo = File::open("/proc/meminfo")?;
+    let reader = BufReader::new(meminfo).lines();
+
+    let mut total = 0;
+    let mut free = 0;
+    let mut buffers = 0;
+    let mut cached = 0;
+
+    for line in reader {
+        let l: Vec<String> = line?.split_whitespace().map(str::to_owned).collect();
+        match l[0].as_str() {
+            "MemTotal:" => {
+                total = l[1].parse::<i32>().unwrap_or(0);
+            }
+            "MemFree:" => {
+                free = l[1].parse::<i32>().unwrap_or(0);
+            }
+            "Buffers" => {
+                buffers = l[1].parse::<i32>().unwrap_or(0);
+            }
+            "Cached:" => {
+                cached = l[1].parse::<i32>().unwrap_or(0);
+                break;
+            }
+            _ => (),
+        }
+    }
+
+    Ok(format!(
+        "{}M / {}M",
+        (total - free - buffers - cached) / 1024,
+        total / 1024
+    ))
 }
