6b0824c4a6e887f16b5647c3a209af54e32a4338
Maxwell Anderson <maxwell.anderson.public@protonmail.com>
2022-03-17 00:23:03 -0600

    implement freebsd memory; change readme image

--- src/mem.rs
+++ src/mem.rs
@@ -7,7 +7,7 @@ use std::{
 #[cfg(target_os = "windows")]
 use crate::{get_output, io};
 
-#[cfg(not(target_os = "windows"))]
+#[cfg(not(any(target_os = "freebsd", target_os = "windows")))]
 pub fn get_mem() -> io::Result<String> {
     let mut used = 0;
     let mut total = 0;
@@ -33,8 +33,35 @@ pub fn get_mem() -> io::Result<String> {
     Ok(format!("{}M / {}M", used / 1024, total / 1024))
 }
 
+#[cfg(target_os = "freebsd")]
+pub fn get_mem() -> io::Result<String> {
+    let total = get_output("sysctl", &["hw.physmem"])?
+        .split_whitespace()
+        .nth(1)
+        .unwrap_or("0")
+        .parse::<i32>()
+        .unwrap_or(0)
+        / 1024;
+
+    format!(
+        "{}M / {}M",
+        total
+            - get_output("vmstat", &["-H"])?
+                .split('\n')
+                .nth(2)
+                .unwrap_or("0")
+                .split_whitespace()
+                .nth(4)
+                .unwrap_or("0")
+                .parse::<i32>()
+                .unwrap_or(0)
+                / 1024,
+        total
+    )
+}
+
 #[cfg(target_os = "windows")]
-pub fn get_mem() -> Result<String, io::Error> {
+pub fn get_mem() -> io::Result<String> {
     let total = get_output("cmd", &["/C", "wmic os get totalvisiblememorysize"])?
         .split('\n')
         .nth(1)
