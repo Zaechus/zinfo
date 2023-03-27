c86acc33009a1a09468c3a9830cb5a2f98a18011
Zaechus <zaechus@pm.me>
2023-03-27 16:44:58 -0600

    use windows crate for hostname

--- src/hostname.rs
+++ src/hostname.rs
@@ -1,11 +1,7 @@
 #[cfg(target_os = "linux")]
-use std::fs;
-
-#[cfg(not(target_os = "linux"))]
-use crate::get_output;
-
-#[cfg(target_os = "linux")]
 pub fn hostname() -> String {
+    use std::fs;
+
     if let Ok(s) = fs::read_to_string("/etc/hostname") {
         s.trim().to_owned()
     } else {
@@ -13,8 +9,38 @@ pub fn hostname() -> String {
     }
 }
 
-#[cfg(not(target_os = "linux"))]
+#[cfg(windows)]
 pub fn hostname() -> String {
+    use windows::{
+        core::PWSTR,
+        Win32::System::SystemInformation::{ComputerNamePhysicalDnsHostname, GetComputerNameExW},
+    };
+
+    let mut buffer_size: u32 = 0;
+    unsafe {
+        GetComputerNameExW(
+            ComputerNamePhysicalDnsHostname,
+            PWSTR::null(),
+            &mut buffer_size,
+        );
+    }
+
+    let mut buffer = vec![0_u16; buffer_size as usize];
+    unsafe {
+        GetComputerNameExW(
+            ComputerNamePhysicalDnsHostname,
+            PWSTR::from_raw(buffer.as_mut_ptr()),
+            &mut buffer_size,
+        );
+    }
+
+    String::from_utf16_lossy(&buffer)
+}
+
+#[cfg(not(any(target_os = "linux", windows)))]
+pub fn hostname() -> String {
+    use crate::get_output;
+
     if let Ok(o) = get_output("hostname", &[]) {
         o
     } else {
